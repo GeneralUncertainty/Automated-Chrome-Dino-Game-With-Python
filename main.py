65469746d7095380eb42af09ad0bb95f76ecf24d
GeneralUncertainty <data.kartikayerishi@gmail.com>
2020-05-29 11:25:52 +0530

    Automated Chrome Dino Game

---
+++ main.py
@@ -0,0 +1,34 @@
+import pyautogui # pip install pyautogui
+from PIL import Image, ImageGrab # pip install pillow
+# from numpy import asarray
+import time
+
+def press(key):
+    pyautogui.keyDown(key)
+    return
+
+def collision(data):
+   
+    for i in range(300, 415):
+        for j in range(410, 563):
+            if data[i, j] < 70:
+                press("down")
+                return
+
+    for i in range(300, 415):
+        for j in range(563, 650):
+            if data[i, j] < 110:
+                press("up")
+                return
+    return
+
+if __name__ == "__main__":
+    print("Hey.. Dino game about to start in 3 seconds")
+    time.sleep(2)
+    
+
+    while True:
+        image = ImageGrab.grab().convert('L')  
+        data = image.load()
+        collision(data)
+            
